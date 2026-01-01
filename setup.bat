a2942984808046e2cf07e317b4e6869d52d0f424
juto222 <thomasjulien3101@gmail.com>
2026-01-01 15:54:18 +0100

    Add pyarmor and nuitka installation to setup.bat

--- setup.bat
+++ setup.bat
@@ -6,7 +6,8 @@ python -m pip install --upgrade pip
 REM Installer les librairies nécessaires
 echo Installation des librairies...
 py -3.11 -m pip install --upgrade requests pynput customtkinter colorama psutil speedtest-cli cx_Freeze clipboard Pillow dnspython bs4 aiohttp
+py -3.11 -m pip install pyarmor==8.5.7
+py -3.11 -m pip install nuitka
 
 echo Toutes les librairies ont été installées.
 pause
-
