14648e8ba1acefc181e84b7f454cd9ac8a73bea7
juto222 <thomasjulien3101@gmail.com>
2026-01-15 08:49:53 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -5,9 +5,11 @@ python -m pip install --upgrade pip
 
 REM Installer les librairies nécessaires
 echo Installation des librairies...
-py -3.11 -m pip install --upgrade requests pynput customtkinter colorama psutil speedtest-cli cx_Freeze clipboard Pillow dnspython bs4 aiohttp
-py -3.11 -m pip install pyarmor==8.5.7
-py -3.11 -m pip install nuitka
+pip install --upgrade requests pynput customtkinter colorama psutil speedtest-cli clipboard Pillow dnspython bs4 aiohttp
+py -3.11 -m pip install cx_Freeze
+pip install pyarmor==8.5.7
+pip install nuitka
 
 echo Toutes les librairies ont été installées.
 pause
+
